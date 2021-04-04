2c8b6461955644f63fac1ca2c9956c1d0311806f
ultrx <fa47753@ubt-uni.net>
2021-04-04 22:05:38 +0200

    End of Section 4

---
+++ Persistence/Seed.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static void SeedData(DataContext context)
+        {//Only executed once when the Program Starts
+            if(!context.Activities.Any())
+            {
+                var activities = new List<Activity>
+                {
+                    new Activity
+                    {
+                        Title = "Past Activity 1",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Activity 2 months ago",
+                        Category = "drinks",
+                    },
+                    new Activity
+                    {
+                        Title = "Past Activity 2",
+                        Date = DateTime.Now.AddMonths(-1),
+                        Description = "Activity 1 month ago",
+                        Category = "culture",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 1",
+                        Date = DateTime.Now.AddMonths(1),
+                        Description = "Activity 1 month in future",
+                        Category = "culture",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 2",
+                        Date = DateTime.Now.AddMonths(2),
+                        Description = "Activity 2 months in future",
+                        Category = "music",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 3",
+                        Date = DateTime.Now.AddMonths(3),
+                        Description = "Activity 3 months in future",
+                        Category = "drinks",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 4",
+                        Date = DateTime.Now.AddMonths(4),
+                        Description = "Activity 4 months in future",
+                        Category = "drinks",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 5",
+                        Date = DateTime.Now.AddMonths(5),
+                        Description = "Activity 5 months in future",
+                        Category = "drinks",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 6",
+                        Date = DateTime.Now.AddMonths(6),
+                        Description = "Activity 6 months in future",
+                        Category = "music",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 7",
+                        Date = DateTime.Now.AddMonths(7),
+                        Description = "Activity 2 months ago",
+                        Category = "travel",
+                    },
+                    new Activity
+                    {
+                        Title = "Future Activity 8",
+                        Date = DateTime.Now.AddMonths(8),
+                        Description = "Activity 8 months in future",
+                        Category = "film",
+                    }
+                };
+
+                context.Activities.AddRange(activities);
+                context.SaveChanges();
+            }
+        }
+    }
+}
